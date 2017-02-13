b4e1dae02cf6fc18cc4f0ab25f575de271f87592
Matt Hart <matthew.hart@linaro.org>
2017-02-13 22:27:55 +0000

    Write .buildpy.cfg file so the kci build script can push builds to the API

--- kernel-builder/set_buildslave_pw.sh
+++ kernel-builder/set_buildslave_pw.sh
@@ -5,6 +5,21 @@ if [ -z "${SSH_PASSWORD}" ]; then
 	echo "=> Please pass your SSH_PASSWORD environment variable"
 	exit 1
 fi
+if [ -z "${API_URL}" ]; then
+	echo "=> Please pass your API_URL environment variable"
+	exit 1
+fi
+if [ -z "${API_TOKEN}" ]; then
+	echo "=> Please pass your API_TOKEN environment variable"
+	exit 1
+fi
+
+
+cat > /home/buildslave/.buildpy.cfg << EOF
+[production]
+url = ${API_URL}
+token = ${API_TOKEN}
+EOF
 
 sed -i "s/BUILDSLAVEPASSWORD/${SSH_PASSWORD}/g" /expect_pw.sh
 /expect_pw.sh
