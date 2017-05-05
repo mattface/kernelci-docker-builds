b5ab8b05c68bd32a8a4dfa7537759a553b58bbb2
Matt Hart <matthew.hart@linaro.org>
2017-05-05 17:19:16 +0100

    Set the buildslave password as an ansible secret
    To keep up with kernelci ansible changes, we need to put the
    build user password into a local secrets file.

--- kernel-builder/ansible-myself.sh
+++ kernel-builder/ansible-myself.sh
@@ -6,4 +6,10 @@ cat > hosts << EOF
 [kernel-builders]
 localhost
 EOF
+mkdir -p group_vars
+cat > group_vars/all << EOF
+ansible_user: root
+become: no
+buildslave_password: ${SSH_PASSWORD}
+EOF
 ansible-playbook -v -i hosts -c local site.yml
